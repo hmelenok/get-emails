5ecc117e7df00119779f3c40cfcc9d9826d26125
Khmel Mykyta <hmelenok@gmail.com>
2019-12-24 17:09:36 +0200

    feat: Update Email regex to latest version

--- test.js
+++ test.js
@@ -15,6 +15,64 @@ test('main', t => {
 	);
 });
 
+test('main with comma separated 50 emails', t => {
+	t.deepEqual(
+		getEmails(fs.readFileSync('fixture-comma-separated.txt', 'utf8')),
+		new Set([
+			'sabren@optonline.net',
+			'grothoff@icloud.com',
+			'bdthomas@aol.com',
+			'rhialto@outlook.com',
+			'darin@msn.com',
+			'marnanel@msn.com',
+			'mxiao@me.com',
+			'laird@mac.com',
+			'jbailie@outlook.com',
+			'hmbrand@live.com',
+			'shazow@yahoo.com',
+			'dobey@msn.com',
+			'bancboy@hotmail.com',
+			'marioph@yahoo.ca',
+			'parrt@comcast.net',
+			'hutton@icloud.com',
+			'jnolan@msn.com',
+			'farber@yahoo.com',
+			'kohlis@msn.com',
+			'shaffei@msn.com',
+			'stinson@hotmail.com',
+			'madler@icloud.com',
+			'jguyer@live.com',
+			'bwcarty@aol.com',
+			'shazow@msn.com',
+			'marin@yahoo.ca',
+			'ryanvm@yahoo.ca',
+			'gregh@msn.com',
+			'akoblin@live.com',
+			'dsugal@sbcglobal.net',
+			'tamas@icloud.com',
+			'bryam@mac.com',
+			'dalamb@att.net',
+			'satishr@att.net',
+			'jkegl@comcast.net',
+			'fhirsch@yahoo.ca',
+			'jaffe@live.com',
+			'mglee@yahoo.ca',
+			'mallanmba@outlook.com',
+			'oster@msn.com',
+			'gozer@att.net',
+			'naoya@yahoo.ca',
+			'sarahs@att.net',
+			'rgarton@hotmail.com',
+			'mlewan@att.net',
+			'stern@live.com',
+			'gavinls@icloud.com',
+			'netsfr@yahoo.com',
+			'gfody@me.com',
+			'multiplx@optonline.net'
+		])
+	);
+});
+
 test('matches correctly', t => {
 	const fixture = `
 		foo bar mailto:sindresorhus@gmail.com unicorn
